8aa5ffec239fbf4857f7daacb492219739f78bf7
FabianCruz14 <fabian.cruzh@alumno.buap.mx>
2024-10-02 14:22:42 -0600

    ENDI24 names deployed in ReactJs

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const players = [
+  {name: "Becerra, E", position: "Setter", height: 173, id: 1},
+  {name: "Perez, M", position: "Outside Hitter", height: 180, id: 2},
+  {name: "Cisneros, R", position: "Setter", height: 168, id: 3},
+  {name: "Villanueva, D", position: "Middle Blocker", height: 185, id: 4},
+  {name: "Ampudia, Y", position: "Middle Blocker", height: 185, id: 5},
+  {name: "Charrin, E", position: "Outside Hitter", height: 180, id: 6},
+  {name: "Wilson, D", position: "Opposite", height: 186, id: 7},
+  {name: "Lara, J", position: "Outside Hitter", height: 179, id: 8},
+  {name: "Alvarez, G", position: "Outside Hitter", height: 182, id: 9},
+  {name: "Cruz, F", position: "Libero", height: 175, id: 10},
+
+]
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const listPlayers = players.map(player=>
+    <li key={player.id}
+    style={{color: player.position === "Libero" ? "grey" : "white"
+    }}
+    >
+      {player.name}, {player.position}, {player.height}
+    </li>
+  );
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ul>
+      {listPlayers}
+    </ul>
+  );
+  
 }
 
 export default App
