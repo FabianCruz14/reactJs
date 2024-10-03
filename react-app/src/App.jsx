d9ea22acdf0d36b88b9caa6cad24f16447f5ef50
FabianCruz14 <fabian.cruzh@alumno.buap.mx>
2024-10-03 17:43:13 -0600

    init tictactoe game

---
+++ react-app/src/App.jsx
@@ -0,0 +1,4 @@
+export default function Square() {
+  // firstly, we create a component that call square who display a one of all squares in the board
+  return <button className="square">X</button>
+}
