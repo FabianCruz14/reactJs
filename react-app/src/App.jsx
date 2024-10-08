8c0c6460d493b9e25fcc8d9e5edbad226a89a17c
FabianCruz14 <fabian.cruzh@alumno.buap.mx>
2024-10-08 01:14:56 -0600

    board whit a fn

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,4 +1,36 @@
-export default function Square() {
+function Square (){
+  return <button className="square">1</button>;
+}
+
+
+export default function Board() {
   // firstly, we create a component that call square who display a one of all squares in the board
-  return <button className="square">X</button>
+  return (
+    <> 
+    {/* we using this sign to create a fragment in the board */}
+    {/* we create a div in who display a one row of the board, nextly create 2 more */}
+
+    {/* create a first row */}
+    <div className="board-row">
+      <Square />
+      <Square />
+      <Square />
+    </div>
+
+    {/* create a second row */}
+    <div className="board-row">
+      <Square />
+      <Square />
+      <Square />
+    </div>
+
+    {/* create a third row */}
+    <div className="board-row">
+      <Square />
+      <Square />
+      <Square />
+    </div>
+
+    </>
+  );
 }
